f5411d8b883c7d2662e47021c48d5061fd7b0952
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-06-26 12:06:17 +0300

    setup mongoDB connection

--- keja-backend/app.js
+++ keja-backend/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Home = require('./models/home');
 
 const app = express();
 
-const homes = []
+// const homes = []
 
 app.use(bodyParser.json());
 
@@ -18,14 +21,12 @@ app.use('/graphql', graphqlHttp({
             name: String!
             homeType: String!
             price: Float!
-            datePosted: String!
         }
 
         input HomeInput {
             name: String!
             homeType: String!
             price: Float!
-            datePosted: String!
         }
     
         type rootQuery{
@@ -44,21 +45,45 @@ app.use('/graphql', graphqlHttp({
     // resolvers (and should have the same name as their type definition)
     rootValue: {
         homes: () => {
-            return homes;
+            return Home.find()
+            .then(homes => {
+                return homes.map(home => {
+                    return {...home._doc};
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
         },
         addHome: (args) =>{
-            const newHome = {
-                _id: Math.random().toString(),
+            const newHome = new Home({
                 name: args.homeInput.name,
                 homeType: args.homeInput.homeType,
                 price: +args.homeInput.price,
-                datePosted: args.homeInput.date,
-            };
-            homes.push(newHome);
-            return newHome;
+                // datePosted: new Date(args.homeInput.date)
+            });
+            // homes.push(newHome);
+            return newHome
+            .save()
+            .then(res => {
+                console.log(res)
+                return {...res._doc};
+            })
+            .catch(err =>{
+                console.log(err)
+                throw err;
+            });
+            // return newHome;
         }
     },
     graphiql: true
 }));
 
-app.listen(3001);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-fhwxu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() =>{
+    // console.log('connected')
+    app.listen(3001);
+})
+.catch(err =>{
+    throw err;
+})
---
+++ keja-backend/models/home.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const homeSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    homeType:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    // datePosted:{
+    //     type: Date,
+    //     required: true
+    // }
+});
+
+module.exports = mongoose.model('Home', homeSchema);
