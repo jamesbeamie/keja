6e145ca9d4e666e8afa4d4229de8b7299017d81a
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-06-26 15:50:54 +0300

    Added relationships

---
+++ keja-backend/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdHomes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Home'
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema);
