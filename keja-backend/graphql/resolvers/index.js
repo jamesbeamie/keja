91114ee03af812c1b195193be2bb9009a7c85ca4
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-07-08 13:00:10 +0300

    Added bookings

--- keja-backend/graphql/resolvers/index.js
+++ keja-backend/graphql/resolvers/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Home = require('../../models/home');
 const User = require('../../models/user');
+const Booking = require('../../models/bookings');
 
 const homes = async homeIds => {
     try{
@@ -19,6 +20,20 @@ const homes = async homeIds => {
         throw  err;
     }
     };
+
+const singleHome = async homeId => {
+    console.log('homeId', homeId);
+    try{
+        const specificHome = await Home.findById(homeId);
+        return {
+            ...specificHome._doc, 
+            _id: specificHome.id,
+            creator: user.bind(this, specificHome.creator)
+        }
+    } catch(err){
+        throw err;
+    }
+}
 
 const user = async userId => {
     try{
@@ -44,6 +59,24 @@ const graphQLresolver = {
                 };
             })
         }catch(err){
+            throw err;
+        }
+    },
+
+    bookings: async () => {
+        try{
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return {
+                    ...booking._doc, 
+                    _id: booking.id,
+                    user: user.bind(this, booking._doc.user),
+                    home: singleHome.bind(this, booking._doc.home)
+                    // createdAt: new Date(booking._doc.createdAt).toISOString, 
+                    // updatedAt: new Date(booking._doc.updatedAt).toISOString
+                }
+            });
+        } catch(err){
             throw err;
         }
     },
@@ -94,6 +127,40 @@ const graphQLresolver = {
     }catch(err){
         throw err;
     };
+    },
+
+    bookHome: async args => {
+        const fetchedHome = await Home.findOne({_id: args.homeId});
+       const booking = new Booking({
+           user:'5d1475e80b6be54627b116a3',
+           home: fetchedHome
+       }); 
+       const result = await booking.save();
+       return {
+           ...result._doc, 
+           _id: result.id,
+           user: user.bind(this, booking._doc.user),
+           home: singleHome.bind(this, booking._doc.home)
+        //    createdAt: new Date(result._doc.createdAt).toISOString, 
+        //    updatedAt: new Date(result._doc.updatedAt).toISOString
+        }
+    },
+
+    cancelBooking: async args => {
+        try{
+            const abooking = await Booking.findById(args.bookingId).populate('home');
+
+        console.log('specificHome', abooking.user);
+            const home = {
+                ...abooking.home._doc, 
+                _id: abooking.home.id,
+                creator: user.bind(this, abooking.user)
+             }
+            await Booking.deleteOne({_id: args.bookingId});
+            return home;
+        }catch (err){
+            throw err;
+        }
     }
 };
 
---
+++ keja-backend/models/bookings.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    home: {
+        type: Schema.Types.ObjectId,
+        ref: 'Home'
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, 
+{ 
+    // timestamps: true
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
