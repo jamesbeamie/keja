033f8b978b35cb9579d3f86d8b39145e65f116db
jamesbeamie <wafulajames9@gmail.com>
2019-08-14 12:57:10 +0300

    implemented context and added page not found

--- keja-frontend/src/commons/header.js
+++ keja-frontend/src/commons/header.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../assets/styling/navbar.css';
+import loginContext from '../containers/context/loginContext';
 
 const Header = () => {
-
 	return (
-		<header className="navigation-bar">
-			<div className="navigation-bar_logo">
-				<h4>keja</h4>
-			</div>
-			<nav className="navigation-bar_links">
-				<ul>
-					<li>
-						<NavLink to="/" exact>
-							Home
-						</NavLink>
-						{' | '}
-						<NavLink to="/homes" exact>
-							Homes
-						</NavLink>
-						{' | '}
-						<NavLink to="/bookings" exact>
-							Bookings
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<loginContext.Consumer>
+			{(context) => {
+				return (
+					<header className="navigation-bar">
+						<div className="navigation-bar_logo">
+							<h4>keja</h4>
+						</div>
+						<nav className="navigation-bar_links">
+							<ul>
+								<li>
+									<NavLink to="/" exact>
+										Home
+									</NavLink>
+									{' | '}
+									<NavLink to="/homes" exact>
+										Homes
+									</NavLink>
+									{' | '}
+									{context.token && (
+										<NavLink to="/bookings" exact>
+											Bookings
+										</NavLink>
+									)}
+								</li>
+							</ul>
+						</nav>
+					</header>
+				);
+			}}
+		</loginContext.Consumer>
 	);
 };
 
